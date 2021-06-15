bad448cae54c89a6c3353f3b806d1c1828e8e4b5
Maulana Yuseph <maulanayuseph@gmail.com>
2021-06-15 12:17:34 +0700

    update repo

---
+++ repo/2-data-table-finished/src/app/course/course.component.ts
@@ -0,0 +1,36 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
+import {debounceTime, distinctUntilChanged, startWith, tap, timeout} from 'rxjs/operators';
+import {merge, fromEvent} from "rxjs";
+
+
+@Component({
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit, AfterViewInit {
+
+    course:Course;
+
+    constructor(private route: ActivatedRoute,
+                private coursesService: CoursesService) {
+
+    }
+
+    ngOnInit() {
+
+        this.course = this.route.snapshot.data["course"];
+
+    }
+
+    ngAfterViewInit() {
+
+    }
+
+
+}
